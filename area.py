f887bc105f25c5b814dc0a05be8e8e4edcb6a776
jenny-smart <jenny@hers.com.tw>
2026-06-20 01:42:03 +0800

    Create area.py

---
+++ area.py
@@ -0,0 +1,16 @@
+AREA_CONFIG = {
+    "台北": {
+        "sheet_id": "1hsmwhA36I0BPXQ8d6OYGGn8R_SETQe4vTR_FB5Sp8Uc",
+        "sheet_name": "新人基本資料",
+        "branch_id": "1",
+        "user_type_id": "2",
+        "coordinator_type": "1",
+    },
+    "台中": {
+        "sheet_id": "1MKadGGTkIefcbPpEVCEPo0SS_wnqsmZSfqjcdhxg6Gk",
+        "sheet_name": "新人基本資料",
+        "branch_id": "2",
+        "user_type_id": "2",
+        "coordinator_type": "1",
+    },
+}
